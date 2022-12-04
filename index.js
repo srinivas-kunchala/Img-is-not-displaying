0d329e04e9badc884893bca6e0dca4d7d67a7603
srinivas <srinivaskunchala122@gmail.com>
2022-12-04 05:25:11 +0000

    image-is-not-displaying

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+
+app.listen(3000);
